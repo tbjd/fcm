4575d106f9942932d855a86ae56d771c36faa64f
Tristan <tristanbrodeur@hotmail.com>
2023-04-04 10:10:34 -0400

    redirect user if already login

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,33 @@
-import type {LinksFunction} from "@remix-run/node";
+import type {LinksFunction, LoaderArgs} from "@remix-run/node";
+import {redirect} from "@remix-run/node";
 import {Link} from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
+import {getUserId} from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
     return [{rel: "stylesheet", href: stylesUrl}];
 };
 
+export const loader = async ({request}: LoaderArgs) => {
+    const userId = await getUserId(request);
+    if (userId) {
+        return redirect("/teams")
+    }
+    return {}
+};
+
 export default function IndexRoute() {
     return (
-        <div >
+        <div>
             <div>
                 <h1>
-                    Log in / register
+                    <Link to="login">Log in / register</Link>
                 </h1>
                 <nav>
                     <ul>
                         <li>
-                            <Link to="teams">By pass</Link>
+                            <Link to="teams">Continue as guess</Link>
                         </li>
                     </ul>
                 </nav>
